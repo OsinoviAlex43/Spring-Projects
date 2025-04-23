a24c3aea2c37d24a56ba391bd28a0f6df375dbb5
OsinoviAlex43 <osinovyjaleksej@gmail.com>
2025-04-23 23:33:25 +0300

    Product-manager/Changing the application.properties

--- product-manager/src/main/java/osinovii/develop/productmanager/entity/Product.java
+++ product-manager/src/main/java/osinovii/develop/productmanager/entity/Product.java
@@ -4,6 +4,7 @@ package osinovii.develop.productmanager.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Short amount;
     private String name;
